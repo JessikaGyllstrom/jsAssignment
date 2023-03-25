464e0a4bc7aa04bcda27419b9fb66181e5f3611a
Jessika Gyllstrom <jessikagyllstrom@gmail.com>
2023-03-26 00:56:26 +0100

    array sorting

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+const myElement = document.getElementById("randomArray");
+
+randomArray();
+// function to randomize an array with 10 unique numbers between 1-100
+function randomArray() {
+    let numbers = new Array(10);
+
+    //iterate through array
+    for (let i = 0; i < numbers.length; i++) {
+    //randomize a number between 0-100
+    let nr = Math.floor(Math.random() * 101);
+
+    const found = numbers.includes(nr);
+
+    if (!found) {
+        numbers[i] = nr;
+        document.getElementById("randomArray").innerHTML += nr + " ";        
+    }
+   
 
 
-numbers = new Array(10);
+    }
+    sortArray(numbers);
+  
+    //sort array of numbers in descending order 
+    function sortArray(numbers) {
+        numbers.join(' ');
 
-// function to randomize a number 
-function randomNumer() {
+          //copy array of numbers
+        const clone = [...numbers];
 
-    // Tar fram ett slumpvärde mellan 0-11
-    var nr = Math.floor(Math.random() * 11);
-    //document.writeln(nr);
-    document.writeln("<br>");
-    document.writeln("Uppdatera sidan för att slumpa fram ett nytt nummer!");
-    numDisplay = "nr";
-    document.getElementById("numDisplay").innerHTML = nr;
 
+        clone.sort(function(a, b){return a - b});
+        document.getElementById("sortedArray").innerHTML += clone.join(' ');        
+
+        
+    }
 }
